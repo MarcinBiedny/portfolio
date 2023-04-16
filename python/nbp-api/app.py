a11246cdaacaca5294698ae4abe29e194d9c68e5
MarcinB <marcin.biedny1@gmail.com>
2023-04-16 13:06:40 +0200

    nbp-api: response status code handling

--- python/nbp-api/app.py
+++ python/nbp-api/app.py
@@ -2,9 +2,13 @@ import requests as r
 import json as j
 
 EXCHANGE_RATE_KEY = 'mid'
+HTTP_STATUS_OK = 200
 
-response = r.get(r'http://api.nbp.pl/api/exchangerates/rates/A/USD/2023-03-07/')
+response = r.get(r'http://api.nbp.pl/api/exchangerates/rates/A/USD/2023-04-09/')
+print(response.status_code)
+if response.status_code == HTTP_STATUS_OK:
+    response_as_json = j.loads(response.text)
+    print('Kurs PLN/USD na dzień '+response_as_json['rates'][0]['effectiveDate']+' wynosi '+str(response_as_json['rates'][0][EXCHANGE_RATE_KEY]))
 
-response_as_json = j.loads(response.text)
-
-print('Kurs PLN/USD na dzień '+response_as_json['rates'][0]['effectiveDate']+' wynosi '+str(response_as_json['rates'][0][EXCHANGE_RATE_KEY]))
+else:
+    print("Something was wrong. Unexpected response status code: " + str(response.status_code))
