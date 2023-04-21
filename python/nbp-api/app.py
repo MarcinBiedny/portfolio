3422b2450f4a505a90c64c5d21b153592d60e346
MarcinB <marcin.biedny1@gmail.com>
2023-04-21 23:51:39 +0200

    implementation of handling days for which the course does not exist

--- python/nbp-api/app.py
+++ python/nbp-api/app.py
@@ -1,8 +1,11 @@
 import requests as r
 import json as j
+import datetime
+
 
 EXCHANGE_RATE_KEY = 'mid'
 HTTP_STATUS_OK = 200
+
 
 currencies_and_tables = {"USD": "A",
                          "AUD": "A",
@@ -13,10 +16,10 @@ currencies_and_tables = {"USD": "A",
                          "MDL": "B",
                          "ARS": "B",}
 
+
 print("This program allows you to check the average rates of the following currencies:: USD, AUD, EUR, CHF, JPN, ZMW (kwacha zambijska), MDL (lej Mołdawii), ARS (peso argentyńskie), ")
 
 currency = input("Please enter the currency you want to check: \n")
-#table = currencies_and_tables[currency.upper()]
 
 while currency.upper() not in currencies_and_tables:
     print("Unfortunately, I do not know such currency.")
@@ -24,17 +27,26 @@ while currency.upper() not in currencies_and_tables:
 
 table = currencies_and_tables[currency.upper()]
 
-#print(currencies_and_tables.get(currency))
-
 date = input("Please enter the date in the format YYYY-MM-DD: ")
+
 url = f"http://api.nbp.pl/api/exchangerates/rates/{table}/{currency}/{date}/"
 response = r.get(url)
 
-#response = r.get(r'http://api.nbp.pl/api/exchangerates/rates/A/USD/2023-04-19/')
-print(response.status_code)
-if response.status_code == HTTP_STATUS_OK:
-    response_as_json = j.loads(response.text)
-    print('Kurs PLN/'+currency.upper()+' na dzień '+response_as_json['rates'][0]['effectiveDate']+' wynosi '+str(response_as_json['rates'][0][EXCHANGE_RATE_KEY]))
+#print(response.status_code)
 
-else:
-    print("Something was wrong. Unexpected response status code: " + str(response.status_code))
+while response.status_code != HTTP_STATUS_OK:
+    new_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    new_date = new_date - datetime.timedelta(days=1)
+    date = new_date.strftime('%Y-%m-%d')
+    url = f"http://api.nbp.pl/api/exchangerates/rates/{table}/{currency}/{date}/"
+    response = r.get(url)
+
+response_as_json = j.loads(response.text)
+print('Kurs PLN/'+currency.upper()+' na dzień '+response_as_json['rates'][0]['effectiveDate']+' wynosi '+str(response_as_json['rates'][0][EXCHANGE_RATE_KEY]))
+
+#if response.status_code == HTTP_STATUS_OK:
+#    response_as_json = j.loads(response.text)
+#    print('Kurs PLN/'+currency.upper()+' na dzień '+response_as_json['rates'][0]['effectiveDate']+' wynosi '+str(response_as_json['rates'][0][EXCHANGE_RATE_KEY]))
+
+#else:
+#    print("Something was wrong. Unexpected response status code: " + str(response.status_code))
