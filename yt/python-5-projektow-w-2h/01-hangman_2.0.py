57925ff08551ce33468097982e291a8d95a286f3
MarcinB <marcin.biedny1@gmail.com>
2023-06-28 21:37:52 +0200

    yt/python-5-projektow-w-2h/01-hangman_2.0.py - do not allow repeated letters - change the entered letters to lowercase - change all letters in the guessed word to lowercase - code formatting

--- yt/python-5-projektow-w-2h/01-hangman_2.0.py
+++ yt/python-5-projektow-w-2h/01-hangman_2.0.py
@@ -1,31 +1,35 @@
 """
 - translate the game into English - check
 - validation (letters only) - check
-- do not allow repeated letters
+- do not allow repeated letters - check
 - word list to guess - check
 - drawing a word from the list (no repetitions in subsequent games) - check
 - select restart/quit game - check
 - selection of the number of chances / game difficulty level - check
 """
 
-import sys, random, json
+import sys
+import random
+import json
+
 
 def difficulty_level():
     while True:
         difficulty = input("Choose your difficulty level: easy/normal/hard.\n")
         if difficulty.lower() == "easy":
-            no_of_tries = int(7)
+            no_of_tries = int(15)
             break
         elif difficulty.lower() == "normal":
-            no_of_tries = int(5)
+            no_of_tries = int(10)
             break
         elif difficulty.lower() == "hard":
-            no_of_tries = int(3)
+            no_of_tries = int(5)
             break
         else:
             print("Wrong difficulty level. Choose again.")
     print("Number of tries: "+str(no_of_tries))
     return no_of_tries
+
 
 def find_indexes(word, letter):
     indexes = []
@@ -36,6 +40,7 @@ def find_indexes(word, letter):
 
     return indexes
 
+
 def show_state_of_game(user_word, no_of_tries, used_letters):
     print()
     print(user_word)
@@ -43,27 +48,33 @@ def show_state_of_game(user_word, no_of_tries, used_letters):
     print("Letters used: ", used_letters)
     print()
 
+
 def random_word():
     with open('yt/python-5-projektow-w-2h/01-hangman_names.json', 'r') as file:
         words = json.load(file)
         word = random.choice(words["names"])
-    return word
+    return word.lower()
 
-def letter_input():
+
+def letter_input(used_letters):
     while True:
-        letter = input("Enter a letter: ")
+        letter = input("Enter a letter: ").lower()
 
         if len(letter) == 1 and letter.isalpha():
-            break
+            if letter not in used_letters:
+                break
+            else:
+                print("You have already entered that letter.")
         else:
             print("It's not a letter.")
     return letter
 
+
 def new_game():
-    
+
     word = random_word()
     used_letters = []
-    user_word =[]
+    user_word = []
     no_of_tries = difficulty_level()
     game_finished = False
 
@@ -71,7 +82,7 @@ def new_game():
         user_word.append("_")
 
     while not game_finished:
-        letter = letter_input()
+        letter = letter_input(used_letters)
         used_letters.append(letter)
 
         found_indexes = find_indexes(word, letter)
@@ -86,12 +97,13 @@ def new_game():
         else:
             for index in found_indexes:
                 user_word[index] = letter
-        
+
             if "".join(user_word) == word:
                 print("Bravo, that's the word!")
                 game_finished = True
-            
+
         show_state_of_game(user_word, no_of_tries, used_letters)
+
 
 print("Welcome to the Hangman game.\nThe game consists in guessing a random male or female name by giving successive letters. Remember that for each misspelled letter you lose one chance.\nGood luck :)")
 while True:
