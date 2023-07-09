67d55ddffd462d0995b324e1823840577914ff9f
MarcinB <marcin.biedny1@gmail.com>
2023-07-09 21:10:39 +0200

    python/tic-tac-toe/classes/board.py - win condition bug fixes

--- python/tic-tac-toe/classes/board.py
+++ python/tic-tac-toe/classes/board.py
@@ -21,11 +21,11 @@ class Board:
     def has_winner(self, symbol):
         if self.area[0][0] == self.area[0][1] == self.area[0][2] == symbol \
                 or self.area[1][0] == self.area[1][1] == self.area[1][2] == symbol \
-                or self.area[2][0] == self.area[2][1] == self.area[0][2] == symbol:
+                or self.area[2][0] == self.area[2][1] == self.area[2][2] == symbol:
             return True
         if self.area[0][0] == self.area[1][0] == self.area[2][0] == symbol \
                 or self.area[0][1] == self.area[1][1] == self.area[2][1] == symbol \
-                or self.area[0][2] == self.area[1][2] == self.area[2][0] == symbol:
+                or self.area[0][2] == self.area[1][2] == self.area[2][2] == symbol:
             return True
         if self.area[0][0] == self.area[1][1] == self.area[2][2] == symbol \
                 or self.area[0][2] == self.area[1][1] == self.area[2][0] == symbol:
