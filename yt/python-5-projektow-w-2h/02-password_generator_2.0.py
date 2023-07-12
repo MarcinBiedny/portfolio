4ec3253058c1e88c65eb0ac20a7f4b741fa8d801
MarcinB <marcin.biedny1@gmail.com>
2023-07-12 21:07:47 +0200

    yt/python-5-projektow-w-2h/02-password_generator_2.0.py - translate into English - remove global variables - validation (option to improve selection): -- always number -- number outside the range -- password length

--- yt/python-5-projektow-w-2h/02-password_generator_2.0.py
+++ yt/python-5-projektow-w-2h/02-password_generator_2.0.py
@@ -1,56 +1,66 @@
 """
-- przetłumaczyć na język angielski
-- walidacja (możliwość poprawy wyboru):
--- zawsze litera
--- liczba z poza przedziału
--- długość hasła
+- translate into English - check
+- remove global variables - check
+- validation (option to improve selection):
+-- always number - check
+-- number outside the range - check
+-- password length - check
 """
 
 import sys, random, string
 
 password = []
-characters_left = -1
+password_lenght = 0
 
-def update_characters_left(number_of_characters):
-    global characters_left
+def correct_data(number):
+    while not number.isdigit():
+        number = input("It's not a number. ")
+    else:
+        return int(number)
 
-    if number_of_characters < 0 or number_of_characters > characters_left:
-        print("Liczba znaków spoza przedziału 0,", characters_left)
-        sys.exit(0)
+def update_characters_left(number_of_characters, characters_left):
+
+    while number_of_characters < 0 or number_of_characters > characters_left:
+        print("Number of characters out od range 0 -", characters_left)
+        number_of_characters = input("Enter a number from the available range. ")
+        number_of_characters = correct_data(number_of_characters)
     else:
         characters_left -= number_of_characters
-        print("Pozostało znaków: ", characters_left)
+        print("Characters left: ", characters_left)
+    return characters_left
 
-password_lenght = int(input("Jak długie ma być hasło? "))
-
-if password_lenght < 5:
-    print("Hasło musi mieć minimum 5 znaków, spróbuj jeszcze raz.")
-    sys.exit(0)
+while password_lenght < 5:
+    password_lenght = input("A good password has at least 5 characters. How long should the password be? ") 
+    password_lenght = correct_data(password_lenght) 
 else:
     characters_left = password_lenght
 
-lowercase_letters = int(input("Ile małych liter ma mieć hasło? "))
-update_characters_left(lowercase_letters)
+lowercase_letters = input("How many lowercase letters should the password have? ")
+lowercase_letters = correct_data(lowercase_letters)
+characters_left = update_characters_left(lowercase_letters, characters_left)
 
-uppercase_letters = int(input("Ile dużych liter ma mieć hasło? "))
-update_characters_left(uppercase_letters)
+uppercase_letters = input("How many uppercase letters should the password have? ")
+uppercase_letters = correct_data(uppercase_letters)
+characters_left = update_characters_left(uppercase_letters, characters_left)
 
-special_characters = int(input("Ile znaków specjalnych ma mieć hasło? "))
-update_characters_left(special_characters)
+special_characters = input("How many special characters should the password have? ")
+special_characters = correct_data(special_characters)
+characters_left = update_characters_left(special_characters, characters_left)
 
-digits = int(input("Ile cyfr ma mieć hasło? "))
-update_characters_left(digits)
+digits = input("How many digits should the password have? ")
+digits = correct_data(digits)
+characters_left = update_characters_left(digits, characters_left)
 
 if characters_left > 0:
-    print("Nie wszystkie znaki zostały wykorzystane. Hasło zostanie uzupełnione małymi literami.")
+    print("Not all characters have been used. The password will be completed with lowercase letters.")
     lowercase_letters += characters_left
 
 print()
-print("Długość hasła: ", password_lenght)
-print("Małe litery: ", lowercase_letters)
-print("Duże litery: ", uppercase_letters)
-print("Znaki specjalne: ", special_characters)
-print("Cyfry: ", digits)
+print("Password lenght: ", password_lenght)
+print("Lowercase letters: ", lowercase_letters)
+print("Uppercase letters: ", uppercase_letters)
+print("Special characters: ", special_characters)
+print("Digits: ", digits)
 
 for _ in range(password_lenght):
     if lowercase_letters > 0:
@@ -67,4 +77,4 @@ for _ in range(password_lenght):
         digits -= 1
 
 random.shuffle(password)
-print("Wygenerowane hasło: ", "".join(password))
+print("Generated password: ", "".join(password))
