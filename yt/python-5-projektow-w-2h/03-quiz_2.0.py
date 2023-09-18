d55ca903f8e4cd2bd90ea558bb783e98f607a069
mb <marcin.biedny1@gmail.com>
2023-09-18 22:02:30 +0200

    yt/python-5-projektow-w-2h/03-quiz_2.0.json - adding a movies category and questions yt/python-5-projektow-w-2h/03-quiz_2.0.py - category selection and validation

--- yt/python-5-projektow-w-2h/03-quiz_2.0.py
+++ yt/python-5-projektow-w-2h/03-quiz_2.0.py
@@ -2,7 +2,7 @@
 - translate into English - check
 - remove global variables - check
 - validation of entered answers - check
-- category selection
+- category selection - check
 - random order of questions - check
 """
 
@@ -10,7 +10,7 @@ import json, random
         
 def show_question(question, points):
     print()
-    print(question["pytanie"])
+    print(question["question"])
     print("a:", question["a"])
     print("b:", question["b"])
     print("c:", question["c"])
@@ -24,21 +24,38 @@ def show_question(question, points):
         else:
             print("Incorrect selection. Choose the answer: a, b, c or d.")
 
-    if answer == question["prawidlowa_odpowiedz"]:
+    if answer == question["correct_answer"]:
         points += 1
         print()
         print("Brawo! This is the correct answer. You already have" , points, "points.")
     else:
         print()
-        print("Unfortunately this is the wrong answer, the correct answer is " + question["prawidlowa_odpowiedz"] + ".")
+        print("Unfortunately this is the wrong answer, the correct answer is " + question["correct_answer"].upper() + ".")
 
     return points
 
 def game():
+
+    print()
+    print("Welcome to the Quiz.")
+    print("The game involves answering questions by selecting the correct answer from among the four available.")
+    print("Good luck :)")
+    print()
+
     points = 0
 
-    with open("yt/python-5-projektow-w-2h/03-quiz_1.0.json") as json_file:
+    with open("yt/python-5-projektow-w-2h/03-quiz_2.0.json") as json_file:
         questions = json.load(json_file)
+
+    while True:
+        available_category = questions.keys()
+        category = input("What category would you like to receive questions from: " + ", ".join(available_category) + "?\n").lower()
+        if category in (available_category):
+            break
+        else:
+            print("There is no such category.")
+
+    questions = questions[str(category)]
 
     random.shuffle(questions)
     for i in range(0, len(questions)):
