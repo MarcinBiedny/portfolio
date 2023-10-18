c33782c95e76180194b135b8eea99f210426cd42
mb <marcin.biedny1@gmail.com>
2023-10-18 19:44:25 +0200

    Create docker local dev env

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from src import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
+    app_host = os.environ.get('FLASK_RUN_HOST','0.0.0.0')
+    app_port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+    app_debug = bool(os.environ.get('FLASK_DEBUG', True))
+
     app.run(debug=True)
     
