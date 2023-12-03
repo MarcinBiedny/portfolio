5d41f362ca757a49507b50b1838f9153722d7a03
mb <marcin.biedny1@gmail.com>
2023-12-03 10:52:02 +0100

    Update .docker/app/entrypoint.sh

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -3,5 +3,6 @@
 # Install any needed packages specified in requirements.txt
 pip install --no-cache-dir -r requirements.txt
 
-# Below command must be always run last
-python app.py
+# Below command must be always last in this file.
+# This keeps containers running.
+tail -f /dev/null
