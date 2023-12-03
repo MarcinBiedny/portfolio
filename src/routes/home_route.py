61c62e8ee4ca5bc43644eb62b42f330501f2fc5f
mb <marcin.biedny1@gmail.com>
2023-12-03 11:11:18 +0100

    'home.route' add '/' route handling

--- src/routes/home_route.py
+++ src/routes/home_route.py
@@ -3,6 +3,7 @@ from src.controllers import home_controller
 
 bp = Blueprint('home', __name__)
 
+@bp.route('/', methods=['GET'])
 @bp.route('/home/', methods=['GET'])
 def route():
     return home_controller.index()
