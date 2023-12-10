9ce8a6e11f53073f55b349a1e7ccaa94c89d0270
mb <marcin.biedny1@gmail.com>
2023-12-10 12:57:42 +0100

    Project Calculator - align 'projects' routing logic

--- src/controllers/projects_controller.py
+++ src/controllers/projects_controller.py
@@ -1,6 +1,11 @@
 from flask import render_template
+from src.models.project_model import Project
 
-def index(project_name, projects):
-    template = 'projects.html' if project_name is None else f'projects/{project_name}/index.html'
+MAIN_PROJECTS_TEMPLATE = "projects.html"
+
+
+def index(project_name):
+    template = MAIN_PROJECTS_TEMPLATE if project_name is None else f'projects/{project_name}/index.html'
+    projects = (Project().get_all()) if (template == MAIN_PROJECTS_TEMPLATE) else None
 
     return render_template(template, projects=projects)
--- src/routes/projects_route.py
+++ src/routes/projects_route.py
@@ -1,12 +1,9 @@
 from flask import Blueprint
 from src.controllers import projects_controller
-from src.models.project_model import Project
 
 bp = Blueprint('projects', __name__)
 
 @bp.route('/projects/', methods=['GET'], defaults={'project_name': None})
 @bp.route('/projects/<project_name>/index.html', methods=['GET'])
 def route(project_name):
-    projects = [] if not project_name is None else Project().get_all()
-
-    return projects_controller.index(project_name, projects)
+    return projects_controller.index(project_name)
