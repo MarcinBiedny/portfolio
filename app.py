c5248d440a5865e17720449b8856b1a8013ac3eb
mb <marcin.biedny1@gmail.com>
2024-03-17 11:50:51 +0100

    app-config-from-object: - app configuration/initialization fixes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from os import getenv
+from dotenv import load_dotenv
+
+# Loading environment variables from '.env file
+# before anu other code
+load_dotenv()
+
 from app import create_app
+from os import getenv
 
-
-app = create_app(getenv("FLASK_ENV", "production"))
 
 if __name__ == "__main__":
+    app = create_app(getenv("FLASK_ENV", "production"))
+
     app_host = getenv("FLASK_RUN_HOST", "0.0.0.0")
     app_port = int(getenv("FLASK_RUN_PORT", 5000))
 
