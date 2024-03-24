a823949afb41c09ac7da598c6ec222002cdc0a4a
mb <marcin.biedny1@gmail.com>
2024-03-24 11:50:11 +0100

    PBJD-25: - app/tests/test_services/test_password_generator.py   - update test logic after PR

--- app/tests/test_services/test_password_generator.py
+++ app/tests/test_services/test_password_generator.py
@@ -5,81 +5,77 @@ from app.services.password_generator import PasswordGenerator
 pg = PasswordGenerator()
 
 
-def test_generate_length():
+def test_password_contains_only_lower():
     for pl in range(8, 25):
-        password = pg.generate(password_length=pl)
+        password = pg.generate(
+            include_uppercase=False,
+            include_numbers=False,
+            include_special_chars=False,
+            password_length=pl,
+        )
+
+        assert_password_correct_len(password=password, expected_len=pl)
+
+        assert all(
+            char.islower() for char in password
+        ), "The generated password should contain only 'lowercase' characters"
+
+
+def test_password_contains_lower_and_special():
+    for pl in range(8, 25):
+        password = pg.generate(
+            include_uppercase=False,
+            include_numbers=False,
+            include_special_chars=True,
+            password_length=pl,
+        )
+
+        assert_password_correct_len(password=password, expected_len=pl)
 
         assert (
-            len(password) == pl
-        ), f"Password should have {pl} characters, but having {len(password)}."
+            any(char.islower() for char in password)
+            and not any(char.isupper() for char in password)
+            and not any(char.isdigit() for char in password)
+            and any(char in string.punctuation for char in password)
+        ), "The generated password should not contain 'uppercase' or 'digit' characters."
 
 
-def test_password_contains_lowercase_letters():
-    password = pg.generate(
-        include_uppercase=True,
-        include_numbers=True,
-        include_special_chars=True,
-        password_length=8,
-    )
+def test_password_does_not_contain_upper():
+    for pl in range(8, 25):
+        password = pg.generate(
+            include_uppercase=False,
+            include_numbers=True,
+            include_special_chars=True,
+            password_length=pl,
+        )
 
-    assert any(
-        char.islower() for char in password
-    ), "Generated password must contain at least one lowercase character."
+        assert_password_correct_len(password=password, expected_len=pl)
 
-
-def test_password_contains_uppercase_letters():
-    password = pg.generate(
-        include_uppercase=True,
-        include_numbers=True,
-        include_special_chars=True,
-        password_length=8,
-    )
-
-    assert any(
-        char.isupper() for char in password
-    ), "Generated password must contain at least one upercase character."
-
-
-def test_password_contains_numbers():
-    password = pg.generate(
-        include_uppercase=True,
-        include_numbers=True,
-        include_special_chars=True,
-        password_length=8,
-    )
-
-    assert any(
-        char.isdigit() for char in password
-    ), "Generated password must contain at least one digit character."
-
-
-def test_password_contains_punctuation():
-    password = pg.generate(
-        include_uppercase=True,
-        include_numbers=True,
-        include_special_chars=True,
-        password_length=8,
-    )
-
-    assert any(
-        char in string.punctuation for char in password
-    ), "Generated password must contain at least one punctuation character."
+        assert (
+            any(char.islower() for char in password)
+            and not any(char.isupper() for char in password)
+            and any(char.isdigit() for char in password)
+            and any(char in string.punctuation for char in password)
+        ), "The generated password should not contain 'uppercase' characters."
 
 
 def test_password_contains_all():
-    password = pg.generate(
-        include_uppercase=True,
-        include_numbers=True,
-        include_special_chars=True,
-        password_length=8,
-    )
+    for pl in range(8, 25):
+        password = pg.generate(
+            include_uppercase=True,
+            include_numbers=True,
+            include_special_chars=True,
+            password_length=pl,
+        )
 
-    assert (
-        any(char.islower() for char in password)
-        and any(char.isupper() for char in password)
-        and any(char.isdigit() for char in password)
-        and any(char in string.punctuation for char in password)
-    ), "The generated password must contain at least: one lowercase letter, one uppercase letter, one number and one special character."
+        assert_password_correct_len(password=password, expected_len=pl)
+
+        assert (
+            any(char.islower() for char in password)
+            and any(char.isupper() for char in password)
+            and any(char.isdigit() for char in password)
+            and any(char in string.punctuation for char in password)
+        ), "The generated password must contain at least:one lowercase letter, one uppercase letter, one number and one special character."
 
 
 def test_password_length_error():
@@ -96,3 +92,12 @@ def test_password_length_error():
     assert (
         str(e.value) == "Password lenght must be between 8 and 24"
     ), "Incorrect error message."
+
+
+# ================== support functions
+
+
+def assert_password_correct_len(password: str, expected_len: int) -> bool:
+    assert (
+        len(password) == expected_len
+    ), f"Password should have {expected_len} characters, but having {len(password)}."
