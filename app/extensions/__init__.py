4207cb41667a70be8d7e362191531ca82df6dc2b
mb <marcin.biedny1@gmail.com>
2024-05-12 13:29:49 +0200

    PBJD-34: - add/config 'Flask-WTF' extension

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask
 from app.extensions.flask_sqlalchemy import db
 from app.extensions.flask_migrate import migrate
 from app.extensions.flask_restx import api_v1
+from app.extensions.flask_wtf import csrf
 
 
 def initialize_extensions(current_app: Flask):
     db.init_app(app=current_app)
     migrate.init_app(app=current_app, db=db)
     api_v1.init_app(app=current_app)
+    csrf.init_app(app=current_app)
