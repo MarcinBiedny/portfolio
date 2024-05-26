676d0675ed1dc8f16d1bbe4a3df1da4beaf879d2
mb <marcin.biedny1@gmail.com>
2024-05-26 12:43:43 +0200

    PBJD-34: - implement Flask-WTF usaged for  'password-generator/index.html' with CSRF protection

--- app/controllers/projects/password_generator_controller.py
+++ app/controllers/projects/password_generator_controller.py
@@ -1,5 +1,23 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
+from app.services.password_generator import PasswordGenerator
+from app.forms.password_generator_form import PasswordGeneratorForm
 
 
 def index():
-    return render_template(url_for("projects.password_generator_index"))
+    form = PasswordGeneratorForm(request.form)
+    password = ""
+
+    if form.validate_on_submit():
+        form_data = request.form.to_dict()
+        password = PasswordGenerator().generate(
+            include_uppercase=bool(form_data.get(form.include_uppercase.name, False)),
+            include_numbers=bool(form_data.get(form.include_numbers.name, False)),
+            include_special_chars=bool(
+                form_data.get(form.include_special_chars.name, False)
+            ),
+            password_length=int(form_data.get(form.password_length.name, 24)),
+        )
+
+    return render_template(
+        url_for("projects.password_generator_index"), form=form, password=password
+    )
